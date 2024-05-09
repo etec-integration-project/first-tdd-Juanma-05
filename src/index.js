273a1045aea224b13339d3c98c7e8d44eb950705
Juanma-05 <j.sancho@alumno.etec.um.edu.ar>
2024-05-09 15:58:12 -0300

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import { createPool } from "mysql2/promise"
+
+const app = express()
+
+const pool = createPool({
+    host: "mysqldb",
+    user: "root",
+    password: "123456",
+    port: 3306
+})
+
+app.get("/", (req, res)=>{
+    res.send("helloword")
+})
+
+app.get("/ping", async(req, res)=>{
+    const result = await pool.query("SELECT NOW()")
+    res.json(result[0])
+})
+
+app.listen(3000)
+console.log("Corriendo")
